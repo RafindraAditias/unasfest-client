12b9632d446e60875e90e608eb9aec973cce8f0e
ilhamSuandi <ilham15suandi@gmail.com>
2023-09-01 21:56:28 +0700

    nambahin section lomba tapi masih ngasal

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,12 @@ import fragment1 from "@/assets/fragments/fragment1.webp";
 import fragment2 from "@/assets/fragments/fragment2.webp";
 import fragment3 from "@/assets/fragments/fragment3.webp";
 import fragment4 from "@/assets/fragments/fragment4.webp";
+import fragment5 from "@/assets/fragments/fragment5.svg";
+import sphere from "@/assets/fragments/sphere.svg";
+import sphere2 from "@/assets/fragments/sphere2.svg";
+import sphere3 from "@/assets/fragments/sphere3.svg";
+import cube from "@/assets/fragments/cube.svg";
+import noImage from "@/assets/icons/noimage.svg";
 
 const Navbar = lazy(() => import("@/components/navbar"));
 const Carousel = lazy(() => import("@/components/carousel"));
@@ -36,8 +42,8 @@ function App() {
     <div className="w-full h-full inline-block font-roboto">
       <Suspense fallback={<div>loading...</div>}>
         <Navbar />
-        <main className="w-screen h-full mt-36">
-          <article className="flex m-auto w-3/4 max-w-[1440px] items-center">
+        <main className="mt-36 max-w-[1200px] m-auto">
+          <section id="beranda" className="flex m-auto items-center">
             <div className="basis-3/4 relative pl-24 pt-24 right-20 bottom-14">
               <img
                 src={fragment1}
@@ -87,8 +93,146 @@ function App() {
               />
               <Carousel images={carouselImages} />
             </div>
-          </article>
+          </section>
           <div className="w-[250px] h-[250px] absolute right-2/3 bottom-0 bg-[#00BF63] rounded-full blur-[350px]"></div>
+
+          {/* TODO */}
+          <div
+            id="galeri"
+            className="w-full h-64 mt-64 border bg-slate-300 rounded-3xl m-auto relative"
+          ></div>
+
+          <section id="lomba" className="relative w-full h-full mt-36">
+            <div className="relative w-full h-full z-10 bg-white rounded-3xl text-center py-24 px-52 mb-36">
+              <img
+                src={sphere}
+                alt="sphere"
+                className="absolute -top-16 -left-16"
+                width={180}
+                height={180}
+              />
+              <img
+                src={fragment5}
+                alt="fragment"
+                className="absolute -top-16 -right-32"
+                width={220}
+                height={220}
+              />
+
+              <img
+                src={sphere2}
+                alt="fragment"
+                className="absolute top-1/3 -left-32"
+                width={220}
+                height={220}
+              />
+
+              <img
+                src={sphere3}
+                alt="fragment"
+                className="absolute bottom-5 -right-32"
+                width={220}
+                height={220}
+              />
+
+              <img
+                src={cube}
+                alt="fragment"
+                className="absolute -bottom-16 left-0"
+                width={220}
+                height={220}
+              />
+              <h1 className="text-5xl font-semibold tracking-normal capitalize mb-4">
+                Lomba Unas Fest
+              </h1>
+              <p className="mb-32">Neque, pulvinar vestibulum non aliquam.</p>
+              <div className="flex gap-16 items-center mb-20">
+                <div className="basis-1/2 h-[500px] grid place-items-center bg-[#F1F1F1]">
+                  <img src={noImage} alt="no image" width={92} height={92} />
+                </div>
+                <div className="basis-1/2">
+                  <h1 className="text-3xl leading-tight font-semibold tracking-normal capitalize text-left mb-8">
+                    Purus id tellus arcu habitant proina magna. Integer purus
+                  </h1>
+                  <p className="text-base text-left mb-8">
+                    Facilisi viverra dictum augue eu lobortis elit. In et donec
+                    habitasseFacilisi viverra dictum augue eu lobortis elit. In
+                    et donec habitasseFacilisi viverra dictum augue eu lobortis
+                    elit. In et donec habitasseFacilisi viverra dictum augue eu
+                    lobortis elit. In et donec habitasseFacilisi viverra dictum
+                    augue eu lobortis elit. In et donec habitasseFacilisi
+                    viverra dictum augue eu lobortis elit. In et donec habitasse
+                  </p>
+                  <div className="flex gap-2">
+                    <button className="bg-[#004AAD] rounded-3xl text-white py-1 px-4 font-semibold">
+                      Daftar Sekarang
+                    </button>
+                    <button className="border-[#004AAD] border-2 rounded-3xl text-[#4E2CA3] py-1 px-4 font-semibold">
+                      Informasi lebih lanjut{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row-reverse gap-16 items-center mb-20">
+                <div className="basis-1/2 h-[500px] grid place-items-center bg-[#F1F1F1]">
+                  <img src={noImage} alt="no image" width={92} height={92} />
+                </div>
+                <div className="basis-1/2">
+                  <h1 className="text-3xl leading-tight font-semibold tracking-normal capitalize text-left mb-8">
+                    Purus id tellus arcu habitant proina magna. Integer purus
+                  </h1>
+                  <p className="text-base text-left mb-8">
+                    Facilisi viverra dictum augue eu lobortis elit. In et donec
+                    habitasseFacilisi viverra dictum augue eu lobortis elit. In
+                    et donec habitasseFacilisi viverra dictum augue eu lobortis
+                    elit. In et donec habitasseFacilisi viverra dictum augue eu
+                    lobortis elit. In et donec habitasseFacilisi viverra dictum
+                    augue eu lobortis elit. In et donec habitasseFacilisi
+                    viverra dictum augue eu lobortis elit. In et donec habitasse
+                  </p>
+                  <div className="flex gap-2">
+                    <button className="bg-[#004AAD] rounded-3xl text-white py-1 px-4 font-semibold">
+                      Daftar Sekarang
+                    </button>
+                    <button className="border-[#004AAD] border-2 rounded-3xl text-[#4E2CA3] py-1 px-4 font-semibold">
+                      Informasi lebih lanjut{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-16 items-center mb-20">
+                <div className="basis-1/2 h-[500px] grid place-items-center bg-[#F1F1F1]">
+                  <img src={noImage} alt="no image" width={92} height={92} />
+                </div>
+                <div className="basis-1/2">
+                  <h1 className="text-3xl leading-tight font-semibold tracking-normal capitalize text-left mb-8">
+                    Purus id tellus arcu habitant proina magna. Integer purus
+                  </h1>
+                  <p className="text-base text-left mb-8">
+                    Facilisi viverra dictum augue eu lobortis elit. In et donec
+                    habitasseFacilisi viverra dictum augue eu lobortis elit. In
+                    et donec habitasseFacilisi viverra dictum augue eu lobortis
+                    elit. In et donec habitasseFacilisi viverra dictum augue eu
+                    lobortis elit. In et donec habitasseFacilisi viverra dictum
+                    augue eu lobortis elit. In et donec habitasseFacilisi
+                    viverra dictum augue eu lobortis elit. In et donec habitasse
+                  </p>
+                  <div className="flex gap-2">
+                    <button className="bg-[#004AAD] rounded-3xl text-white py-1 px-4 font-semibold">
+                      Daftar Sekarang
+                    </button>
+                    <button className="border-[#004AAD] border-2 rounded-3xl text-[#4E2CA3] py-1 px-4 font-semibold">
+                      Informasi lebih lanjut{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[450px] h-[450px] absolute -right-[50px] -top-[50px] bg-[#00BF63] rounded-full blur-[350px]"></div>
+            <div className="w-[450px] h-[450px] absolute -left-[50px] top-20 bg-[#FFDE59] rounded-full blur-[350px]"></div>
+            <div className="w-[450px] h-[450px] absolute -left-[50px] bottom-20 bg-[#00BF63] rounded-full blur-[350px]"></div>
+            <div className="w-[450px] h-[450px] absolute -right-[50px] bottom-1/3 bg-[#6CC8FC] rounded-full blur-[350px]"></div>
+          </section>
         </main>
       </Suspense>
     </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,41 @@
 import graphicLogo from "@/assets/logo/graphic-logo.png";
 import unasLogo from "@/assets/logo/logo-unasfest.png";
+import { useState } from "react";
+import { Link } from "react-scroll";
 
 function Navbar() {
+  const [currentNav, setCurrentNav] = useState<string>("Beranda");
+
+  const navContents = [
+    {
+      path: "beranda",
+      name: "Beranda",
+    },
+    {
+      path: "lomba",
+      name: "Lomba",
+    },
+    {
+      path: "galeri",
+      name: "Galeri",
+    },
+    {
+      path: "sponsorship",
+      name: "Sponsorship",
+    },
+    {
+      path: "ranking",
+      name: "Ranking",
+    },
+    {
+      path: "tentang",
+      name: "Tentang",
+    },
+  ];
+
   return (
     <header className="w-screen h-[80px] fixed z-50 bg-white border-b-2 border-solid border-[rgba(0, 0, 0, 0.10)]">
-      <nav className="w-3/4 max-w-[1440px] h-full flex items-center justify-between gap-10 m-auto">
+      <nav className="w-full max-w-[1200px] h-full flex items-center justify-between gap-10 m-auto">
         <div className="flex w-40 h-12 gap-2 ml-5">
           <img
             src={graphicLogo}
@@ -24,12 +55,23 @@ function Navbar() {
 
         <div>
           <ul className="flex gap-12 font-roboto font-semibold text-base tracking-wide mr-5">
-            <li>Beranda</li>
-            <li>Lomba</li>
-            <li>Galeri</li>
-            <li>Sponsorship</li>
-            <li>Ranking</li>
-            <li>Tentang</li>
+            {navContents.map(({ path, name }, index) => {
+              return (
+                <li key={index}>
+                  <Link
+                    to={path}
+                    spy={true}
+                    smooth={true}
+                    offset={-200}
+                    duration={50}
+                    onClick={() => setCurrentNav(name)}
+                    className={`${currentNav === name ? "nav-active" : "nav"}`}
+                  >
+                    {name}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
